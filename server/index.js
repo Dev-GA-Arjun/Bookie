8d2a8ef61ef28d6c3d7840b76ebf1c41354541fa
G A ARJUN <gaarjun00@gmail.com>
2026-09-17 07:04:32 +0530

    chore: wire auth routes with express app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import 'dotenv/config';
+import authRoutes from "./src/routes/auth.routes.js";
 
 const app = express()
 app.use(express.json())
 
-app.get("/", async (req,res) => {
-  res.send("Welcome to the Blogify API! This is the main entry point.")
-})
 
-app.listen(3000, () => {console.log("Running in http://localhost:3000/")});
+app.use('/api/v1/auth', authRoutes);
+app.get("/", async (req,res) => {
+  res.send("Welcome to the Bookie API! This is the main entry point.")
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {console.log(`Running in http://localhost:${PORT}/`)});
